413d99f078f39c85179682c06f8f478c1c525347
sogern <sogern@gmail.com>
2017-01-26 15:40:14 +0100

    Updated ChronicleUtil, util.stg and Chronicle tests. Cycle scan for number of excerpts is now O(log N). Refactored Chronicle test cases. Bugfixes in ChronicleUtil.

--- src/test/java/com/nextgen/tests/ChronicleTestSession.java
+++ src/test/java/com/nextgen/tests/ChronicleTestSession.java
@@ -18,12 +18,16 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 import static com.nextgen.tests.util.ChronicleUtil.*;
 
@@ -31,7 +35,16 @@ public class ChronicleTestSession {
 
 	private static final Logger log = LoggerFactory.getLogger(ChronicleTestSession.class);
 
+	private static final int NUM_RECORDS = 10000;
+
+	// 5 seconds total running time - so we get a few roll cycles in Chronicle
+	private static final int CHILL_TIME = 5000d / NUM_RECORDS < 1.0d ? 1 : 5000 / NUM_RECORDS;
+
 	private static String basePath;
+
+	private enum SearchType {
+		BINARY, SEQUENTIAL
+	}
 
 	@BeforeClass
 	public static void setup(TestContext context) {
@@ -42,6 +55,7 @@ public class ChronicleTestSession {
 	public RunTestOnContext rule = new RunTestOnContext();
 
 	protected class TestSession {
+
 		private final ChronicleRecord<MarketData> marketDataRecord;
 		private final TestContext context;
 		protected final Async async;
@@ -104,14 +118,15 @@ public class ChronicleTestSession {
 			if (file.exists() && file.isDirectory()) {
 				Arrays.asList(file.list())
 					.stream()
-					.filter(s -> new File(s).toString().endsWith(".cq4")).map(s1 -> new File(String.format("%s/%s", basePath, s1))).forEach(f -> {
-					if (!f.delete()) {
-						log.error("Could not delete " + f);
-						context.fail("Could not delete " + f);
-					}
-					else log.info("Removed " + f);
+					.filter(s -> s.endsWith(".cq4"))
+					.map(s -> new File(String.format("%s/%s", basePath, s)))
+					.forEach(f -> {
+						if (!f.delete()) {
+							log.error("Could not delete " + f);
+							context.fail("Could not delete " + f);
+						} else log.info("Removed " + f);
 
-				});
+					});
 			}
 			else if (file.exists() && file.delete()) {
 				log.error("Could not delete " + file);
@@ -138,25 +153,27 @@ public class ChronicleTestSession {
 			final AtomicInteger written = new AtomicInteger(0);
 
 			//
-			final int numEntries = 1000;
-			final int chillTime = 5000 / numEntries;	// 5 seconds total running time - so we get a few roll cycles in Chronicle
+			final int debugOutputPer = NUM_RECORDS / 10;
 
-			IntStream.generate(() -> ThreadLocalRandom.current().nextInt(105900, 106000)).limit(numEntries).forEach(i -> {
+			log.debug("CHILL_TIME: " + CHILL_TIME);
+
+			IntStream.generate(() -> ThreadLocalRandom.current().nextInt(105900, 106000)).limit(NUM_RECORDS).forEach(i -> {
 				marketData
-					.setTimestamp(timestamp.plusSeconds(written.get()).toInstant().toEpochMilli())
-					.setVolume(ThreadLocalRandom.current().nextLong(1, 11) * 1000000l)
+					.setTimestamp(timestamp.plusSeconds(written.getAndIncrement()).toInstant().toEpochMilli())
+					.setVolume(ThreadLocalRandom.current().nextLong(1, 21) * 1000000l)
 					.setBid(i)
 					.setOffer(i + 10);
 
 				appender.writeDocument(wireOut -> marketDataRecord.write(marketData, wireOut));
 
-				if (written.incrementAndGet() % 100 == 0 || written.get() < 11 || written.get() >= 990) {
+				// Debug output of the first and last 10 records + every NUM_RECORDS / 10 record
+				if (written.get() % debugOutputPer == 0 || written.get() <= 11 || written.get() >= NUM_RECORDS - 10) {
 					log.debug(written.get() + ": lastIndexAppended: " + appender.lastIndexAppended() + ", seq_num: " + rollCycle.toSequenceNumber(appender.lastIndexAppended()));
 					debugMarketData(marketData, written);
 				}
 
 				try {
-					Thread.currentThread().sleep(chillTime);
+					Thread.currentThread().sleep(CHILL_TIME);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -172,7 +189,8 @@ public class ChronicleTestSession {
 
 			while (tailer.readDocument(wireIn -> marketDataRecord.read(wireIn, marketData))) {
 
-				if (read.incrementAndGet() % 100 == 0 || read.get() < 11 || read.get() >= 990) {
+				// Debug output of the first and last 10 records + every NUM_RECORDS / 10 record
+				if (read.incrementAndGet() % debugOutputPer == 0 || read.get() < 11 || read.get() >= NUM_RECORDS - 10) {
 					log.debug(read.get() + ": index: " + tailer.index() + ", seq_num: " + rollCycle.toSequenceNumber(tailer.index()));
 					debugMarketData(marketData, read);
 				}
@@ -183,76 +201,92 @@ public class ChronicleTestSession {
 			after.handle(sb.toString());
 		}
 
-		public void doSequentialSearchTest(Handler<String> after) {
+		private void doSearchTest(final SearchType type, final Stream<Long> timestamps, BiConsumer<MarketData, Long> checkResult) {
 			SingleChronicleQueue queue = getChronicleQueue();
-
-			final ZonedDateTime timestamp = getZonedDateTime();
 
 			final MarketData marketData = new MarketData();
 
 			final ExcerptTailer tailer = queue.createTailer();
 
-			IntStream.generate(() -> ThreadLocalRandom.current().nextInt(0, 1000)).limit(1).forEach(recordNum -> {
-				long searchFor = timestamp.plusSeconds(recordNum).toInstant().getEpochSecond() * 1000;
+			timestamps.forEach(searchFor -> {
+				long result = 0;
 
-				long result = sequentialExcerptSearch(queue, searchFor, marketDataRecord);
-				long result2 = sequentialExcerptSearchReverse(queue, searchFor, marketDataRecord);
+				switch (type) {
 
-				context.assertEquals(result, result2, "forward and reverse search did not hit same result");
+					case BINARY: {
+						result = binaryExcerptSearch(queue, searchFor, marketDataRecord);
+						break;
+					}
 
-				log.info("Search for " + searchFor + " hit: " + result);
+					case SEQUENTIAL: {
+						result = sequentialExcerptSearch(queue, searchFor, marketDataRecord);
+						long result2 = sequentialExcerptSearchReverse(queue, searchFor, marketDataRecord);
+						context.assertEquals(result, result2, "forward and reverse search did not hit same result");
+						break;
+					}
+				}
+
+				log.info("Search for " + searchFor + " result: " + result);
 
 				context.assertTrue(result > 0, "search failed: " + result);
-				context.assertTrue(tailer.moveToIndex(result), "erroneous index from search result: " + result);
+
+				final boolean success = tailer.moveToIndex(result);
+
+				context.assertTrue(success, "erroneous index from search result: " + result);
 
 				tailer.readDocument(wireIn -> marketDataRecord.read(wireIn, marketData));
 
 				log.debug("Search: index: " + tailer.index() + ", seq_num: " + queue.rollCycle().toSequenceNumber(tailer.index()));
-				debugMarketData(marketData, new AtomicInteger(recordNum));
+				debugMarketData(marketData, new AtomicInteger(0));
 
-				context.assertTrue(marketData.getTimestamp() == searchFor, "did not hit exact record");
+				checkResult.accept(marketData, searchFor);
 			});
 
 			queue.close();
+		}
 
-			after.handle(marketData.toString());
+		private void doRandomSearchTest(final SearchType type, Handler<String> after) {
+
+			final ZonedDateTime timestamp = getZonedDateTime();
+
+			doSearchTest(
+				type,
+				LongStream.generate(() -> ThreadLocalRandom.current().nextLong(0, NUM_RECORDS))
+					.limit(1)
+					.map(recordNum -> timestamp.plusSeconds(recordNum).toInstant().toEpochMilli())
+					.boxed(),
+				(marketData, searchFor) -> context.assertTrue(marketData.getTimestamp() == searchFor, "did not hit exact record")
+			);
+
+			after.handle("PASSED");
+		}
+
+		private void doSearchTest(final SearchType type, Long timestamp, Handler<String> after) {
+
+			doSearchTest(
+				type,
+				LongStream.builder().add(timestamp).build().boxed(),
+				(marketData, searchFor) -> context.assertTrue(marketData.getTimestamp() != 0, "did not find any records")
+			);
+
+			after.handle("PASSED");
+		}
+
+		public void doSequentialSearchTest(Handler<String> after) {
+			doRandomSearchTest(SearchType.SEQUENTIAL, after);
 		}
 
 		public void doBinarySearchTest(Handler<String> after) {
-			SingleChronicleQueue queue = getChronicleQueue();
+			doRandomSearchTest(SearchType.BINARY, after);
+		}
 
-			final ZonedDateTime timestamp = getZonedDateTime();
-
-			final MarketData marketData = new MarketData();
-
-			final ExcerptTailer tailer = queue.createTailer();
-
-			IntStream.generate(() -> ThreadLocalRandom.current().nextInt(0, 1000)).limit(1).forEach(recordNum -> {
-				long searchFor = timestamp.plusSeconds(recordNum).toInstant().getEpochSecond() * 1000;
-
-				long result = binaryExcerptSearch(queue, searchFor, marketDataRecord);
-
-				log.info("Search for " + searchFor + " hit: " + result);
-
-				context.assertTrue(result > 0, "search failed: " + result);
-				context.assertTrue(tailer.moveToIndex(result), "erroneous index from search result: " + result);
-
-				tailer.readDocument(wireIn -> marketDataRecord.read(wireIn, marketData));
-
-				log.debug("Search: index: " + tailer.index() + ", seq_num: " + queue.rollCycle().toSequenceNumber(tailer.index()));
-				debugMarketData(marketData, new AtomicInteger(recordNum));
-
-				context.assertTrue(Math.abs(marketData.getTimestamp() - searchFor) < 10000, "not close enough");
-			});
-
-			queue.close();
-
-			after.handle(marketData.toString());
+		public void doBinarySearchTest(Long timestamp, Handler<String> after) {
+			doSearchTest(SearchType.BINARY, timestamp, after);
 		}
 	}
 
 	private static void debugMarketData(MarketData marketData, AtomicInteger i) {
 		LocalDateTime ldt = LocalDateTime.ofInstant(Instant.ofEpochMilli(marketData.getTimestamp()), ZoneId.of("CET"));
-		log.debug(i.get() + " [" + ldt + "]: " + marketData);
+		log.debug(i.get() + " [" + ldt.format(DateTimeFormatter.ISO_DATE_TIME) + "]: " + marketData);
 	}
 }
