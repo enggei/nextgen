efd88c60977ecdc2a42249443c6d195b757253fd
geirove <geiroveenge@gmail.com>
2017-09-24 11:25:39 +0200

    Added Domain-visitor support. Added MobX. Fixed issue in Tree-Layout (faster and more efficient)

---
+++ src/main/java/com/generator/generators/mobx/MobXAppVisitor.java
@@ -0,0 +1,46 @@
+package com.generator.generators.mobx;
+
+import com.generator.app.App;
+import com.generator.app.AppMotif;
+import com.generator.generators.domain.DomainVisitor;
+import com.generator.generators.project.ProjectPlugin;
+import com.generator.util.NeoUtil;
+import org.neo4j.graphdb.Node;
+
+import java.util.Stack;
+
+/**
+ * Created 22.09.17.
+ */
+public class MobXAppVisitor extends DomainVisitor {
+
+   private final MobXGroup mobXGroup = new MobXGroup();
+   private final Stack<MobXGroup.ModelST> modelStack = new Stack<>();
+
+   public MobXAppVisitor(Node visitorNode, App app) {
+      super(true,visitorNode, app);
+   }
+
+   @Override
+   public void visitEntity(Node node) {
+
+      if(!modelStack.isEmpty()) return;
+      System.out.println("MobX Entity " + NeoUtil.getNameAndLabelsFrom(node));
+
+      final MobXGroup.ModelST modelST = mobXGroup.newModel().
+            setName(NeoUtil.get(node, AppMotif.Properties.name.name()));
+      modelStack.push(modelST);
+      super.visitEntity(node);
+
+      NeoUtil.outgoing(visitorNode, ProjectPlugin.Relations.RENDERER).forEach(relationship -> ProjectPlugin.renderToFile(relationship, null, modelST.toString(), NeoUtil.other(visitorNode, relationship), app));
+      System.out.println(modelST);
+   }
+
+   @Override
+   public void visitProperty(Node node) {
+      System.out.println("MobX Property " + NeoUtil.getNameAndLabelsFrom(node));
+      super.visitProperty(node);
+
+      modelStack.peek().addObservablesValue(null, NeoUtil.get(node, AppMotif.Properties.name.name()));
+   }
+}
