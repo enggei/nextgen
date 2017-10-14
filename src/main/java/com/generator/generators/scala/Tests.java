d045f2463e5078a152a1a78be279b8c8042f23f5
sogern <sogern@gmail.com>
2017-10-14 10:07:17 +0200

    Scala: back to basics. Work in progress.

--- src/main/java/com/generator/generators/scala/Tests.java
+++ src/main/java/com/generator/generators/scala/Tests.java
@@ -1,9 +1,9 @@
 package com.generator.generators.scala;
 
 import com.generator.ProjectConstants;
-import com.generator.generators.scala.parser.ScalaLexer;
-import com.generator.generators.scala.parser.ScalaNodeListener;
-import com.generator.generators.scala.parser.ScalaParser;
+//import com.generator.generators.scala.parser.ScalaLexer;
+//import com.generator.generators.scala.parser.ScalaNodeListener;
+//import com.generator.generators.scala.parser.ScalaParser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
@@ -15,8 +15,13 @@ public class Tests {
 
 	@Test
 	public void testScala() throws IOException {
-		final ScalaNodeListener listener = new ScalaNodeListener(true);
-		ScalaParser scalaParser = new ScalaParser(new CommonTokenStream(new ScalaLexer(CharStreams.fromFileName(ProjectConstants.MAIN_ROOT + "/com/generator/generators/scala/Test.scala"))));
-		new ParseTreeWalker().walk(listener, scalaParser.compilationUnit());
+//		final ScalaNodeListener listener = new ScalaNodeListener(true);
+//		ScalaParser scalaParser = new ScalaParser(new CommonTokenStream(new ScalaLexer(CharStreams.fromFileName(ProjectConstants.MAIN_ROOT + "/com/generator/generators/scala/QualId.scala"))));
+//
+//		ScalaParser.ProgramContext context = scalaParser.program();
+//		new ParseTreeWalker().walk(listener, context);
+
+//		ScalaParser.CompilationUnitContext compilationUnit = scalaParser.compilationUnit();
+//		new ParseTreeWalker().walk(listener, compilationUnit);
 	}
 }
