89d4ab74811399ca507ffd57a79c62ff0ffef95f
geirove <geiroveenge@gmail.com>
2017-10-18 10:09:10 +0200

    Added Rivescript-test of running bot

--- src/main/java/com/generator/generators/rivescript/Tests.java
+++ src/main/java/com/generator/generators/rivescript/Tests.java
@@ -1,11 +1,52 @@
 package com.generator.generators.rivescript;
 
+import com.rivescript.ConcatMode;
+import com.rivescript.Config;
+import com.rivescript.RiveScript;
+import com.rivescript.session.ConcurrentHashMapSessionManager;
+import com.rivescript.session.SessionManager;
 import org.junit.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created 13.10.17.
  */
 public class Tests {
+
+   public static void main(String[] args) {
+
+      // Create a new bot with the default settings.
+      final SessionManager sessionManager = new ConcurrentHashMapSessionManager();
+
+      final Map<String, String> errors = new LinkedHashMap<>();
+
+      RiveScript bot = new RiveScript(Config.newBuilder()
+            .throwExceptions(false)          // Whether exception throwing is enabled
+            .strict(true)                    // Whether strict syntax checking is enabled
+            .utf8(false)                     // Whether UTF-8 mode is enabled
+            .unicodePunctuation("[.,!?;:]")  // The unicode punctuation pattern
+            .forceCase(false)                // Whether forcing triggers to lowercase is enabled
+            .concat(ConcatMode.NONE)         // The concat mode
+            .depth(50)                       // The recursion depth limit
+            .sessionManager(sessionManager)  // The session manager for user variables
+            .errorMessages(errors)           // Map of custom error messages
+            .build());
+
+      // Load a directory full of RiveScript documents (.rive files)
+      bot.loadDirectory("src/main/java/com/generator/generators/rivescript/replies");
+
+      // Load an individual file.
+//      bot.loadFile("src/main/java/com/generator/generators/rivescript/replies/eliza.rive");
+
+      // Sort the replies after loading them!
+      bot.sortReplies();
+
+      // Get a reply.
+      String reply = bot.reply("user", "Hello bot!");
+      System.out.println(reply);
+   }
 
    @Test
    public void testGroup() {
