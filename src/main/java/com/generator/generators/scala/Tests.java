6304e20705ff1d7654835103636121edb4b0512a
sogern <sogern@gmail.com>
2017-10-22 20:59:55 +0200

    Minor refactoring

--- src/main/java/com/generator/generators/scala/Tests.java
+++ src/main/java/com/generator/generators/scala/Tests.java
@@ -27,33 +27,38 @@ public class Tests {
 	}
 
 	@Test
+	@SuppressWarnings("unchecked")
 	public void testScalaParser() throws IOException {
 
-//		testParserRule(ScalaNodeListener.class, ScalaLexer.class, ScalaParser.class, parser -> ((ScalaParser)parser).qualId(),
+		Class nlc = ScalaNodeListener.class;
+		Class lc = ScalaLexer.class;
+		Class pc = ScalaParser.class;
+
+//		testParserRule(nlc, lc, pc, parser -> ((ScalaParser)parser).qualId(),
 //			"xx_=",
 //			true);
 //
-//		testParserRule(ScalaNodeListener.class, ScalaLexer.class, ScalaParser.class, parser -> ((ScalaParser)parser).compilationUnit(),
+//		testParserRule(nlc, lc, pc, parser -> ((ScalaParser)parser).compilationUnit(),
 //			fileSystem.getPath(workDir + "Test.scala"),
 //			true);
 //
-//		testParserRule(ScalaNodeListener.class, ScalaLexer.class, ScalaParser.class, parser -> ((ScalaParser)parser).dcl(),
+//		testParserRule(nlc, lc, pc, parser -> ((ScalaParser)parser).dcl(),
 //			"def xx_= (yy: TT): Unit { }",
 //			true);
 //
-//		testParserRule(ScalaNodeListener.class, ScalaLexer.class, ScalaParser.class, parser -> ((ScalaParser)parser).compilationUnit(),
+//		testParserRule(nlc, lc, pc, parser -> ((ScalaParser)parser).compilationUnit(),
 //			"package P {  object X { val x = 1; val y = 2 } }",
 //			true);
 //
-//		testParserRule(ScalaNodeListener.class, ScalaLexer.class, ScalaParser.class, parser -> ((ScalaParser)parser).templateBody(),
+//		testParserRule(nlc, lc, pc, parser -> ((ScalaParser)parser).templateBody(),
 //			"{ val x = 1; val y = 2 }",
 //			true);
 //
-//		testParserRule(ScalaNodeListener.class, ScalaLexer.class, ScalaParser.class, parser -> ((ScalaParser)parser).ids(),
+//		testParserRule(nlc, lc, pc, parser -> ((ScalaParser)parser).ids(),
 //			"x, Object, maxIndex, p2p, empty_?, `yield`, αρετη, _y, dot_product_*, __system, _MAX_LEN_",
 //			true);
 
-		testParserRule(ScalaNodeListener.class, ScalaLexer.class, ScalaParser.class, parser -> ((ScalaParser)parser).compilationUnit(),
+		testParserRule(nlc, lc, pc, parser -> ((ScalaParser)parser).compilationUnit(),
 			"object Test { def main(args: Array[String]): Unit = { print(\"Hello world!\"); } }",
 			true);
 	}
--- src/main/java/com/generator/util/AntlrTestUtil.java
+++ src/main/java/com/generator/util/AntlrTestUtil.java
@@ -3,6 +3,7 @@ package com.generator.util;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.lang.reflect.Constructor;
@@ -19,15 +20,24 @@ public class AntlrTestUtil {
 		C apply(P parser);
 	}
 
+	private static Class getClass(final Package pkg, final String name) {
+		try {
+			return Class.forName(pkg.getName() + "." + name);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		throw new RuntimeException("Class " + pkg.getName() + "." + name + " was not found");
+	}
+
 	private static Constructor getConstructor(final Class clazz, final String argType) {
 		for (Constructor ctor : clazz.getDeclaredConstructors()) {
 
-			if (ctor.getParameterCount() == 1) {
+			if (ctor.getParameterCount() != 1) continue;
 
-				if (ctor.getGenericParameterTypes()[0].getTypeName().equals(argType)) {
-					ctor.setAccessible(true);
-					return ctor;
-				}
+			if (ctor.getGenericParameterTypes()[0].getTypeName().equals(argType)) {
+				ctor.setAccessible(true);
+				return ctor;
 			}
 		}
 
@@ -35,6 +45,7 @@ public class AntlrTestUtil {
 	}
 
 	@SuppressWarnings("unchecked")
+	@NotNull
 	private static <X extends Lexer> X newLexer(final Class<X> clazz, CharStream charStream) {
 		Constructor ctor = getConstructor(clazz, CharStream.class.getName());
 
@@ -48,6 +59,7 @@ public class AntlrTestUtil {
 	}
 
 	@SuppressWarnings("unchecked")
+	@NotNull
 	private static <P extends Parser> P newParser(final Class<P> clazz, TokenStream tokenStream) {
 		Constructor ctor = getConstructor(clazz, TokenStream.class.getName());
 
@@ -61,6 +73,7 @@ public class AntlrTestUtil {
 	}
 
 	@SuppressWarnings("unchecked")
+	@NotNull
 	private static <L extends ParseTreeListener> L newParseTreeListener(final Class<L> clazz, boolean debug) {
 		Constructor ctor = getConstructor(clazz, "boolean");
 
@@ -73,6 +86,11 @@ public class AntlrTestUtil {
 		throw new RuntimeException("Constructor " + clazz.getName() + "(" + TokenStream.class.getName() + ") was not found");
 	}
 
+	@NotNull
+	private static <X extends Lexer> CommonTokenStream newTokenStream(Class<X> lexerClass, CharStream charStream) {
+		return new CommonTokenStream(newLexer(lexerClass, charStream));
+	}
+
 	public static <L extends ParseTreeListener, X extends Lexer, P extends Parser> void testParserRule(Class<L> listenerClass, Class<X> lexerClass, Class<P> parserClass, RuleTester ruleContextFunction, final String input, boolean debug) throws IOException {
 		testParserRule(listenerClass, lexerClass, parserClass, ruleContextFunction, CharStreams.fromString(input), debug);
 	}
@@ -83,18 +101,14 @@ public class AntlrTestUtil {
 
 	@SuppressWarnings("unchecked")
 	public static <L extends ParseTreeListener, X extends Lexer, P extends Parser> void testParserRule(Class<L> listenerClass, Class<X> lexerClass, Class<P> parserClass, RuleTester ruleContextFunction, CharStream charStream, boolean debug) throws IOException {
-
-		ParserRuleContext context = ruleContextFunction.apply(
-			newParser(
-				parserClass,
-				new CommonTokenStream(
-					newLexer(lexerClass, charStream)
+		new ParseTreeWalker().walk(
+			newParseTreeListener(listenerClass, debug),
+			ruleContextFunction.apply(
+				newParser(
+					parserClass,
+					newTokenStream(lexerClass, charStream)
 				)
 			)
-		);
-
-		new ParseTreeWalker().walk(
-			newParseTreeListener(listenerClass, debug), context
 		);
 	}
 
