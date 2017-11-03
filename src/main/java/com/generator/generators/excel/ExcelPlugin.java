ab7367f9957e44576250df8e2a4a38de9d3cba46
geirove <geiroveenge@gmail.com>
2017-11-03 11:38:23 +0100

    added Sheet-editor for Excel

--- src/main/java/com/generator/generators/excel/ExcelPlugin.java
+++ src/main/java/com/generator/generators/excel/ExcelPlugin.java
@@ -3,24 +3,24 @@ package com.generator.generators.excel;
 import com.generator.app.App;
 import com.generator.app.nodes.NeoNode;
 import com.generator.generators.project.ProjectPlugin;
-import com.generator.util.NeoUtil;
+import com.generator.neo.NeoModel;
 import com.generator.util.SwingUtil;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
 
 import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
+import java.util.*;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created 03.11.17.
@@ -79,7 +79,7 @@ public class ExcelPlugin extends ExcelDomainPlugin {
                      if ("".equals(ExcelPlugin.getValue(cellNode))) return;
 
                      incomingCELL(cellNode, (cellColumnRelation, other) -> {
-                        if(isColumn(other)) {
+                        if (isColumn(other)) {
                            final XSSFCell cell = row.createCell(Integer.valueOf(getNameProperty(other)));
                            cell.setCellValue((String) ExcelPlugin.getValue(cellNode));
                         }
@@ -207,5 +207,187 @@ public class ExcelPlugin extends ExcelDomainPlugin {
             setValue(cellNode.getNode(), value);
          }
       });
+   }
+
+   @Override
+   protected JComponent newSheetEditor(NeoNode sheetNode) {
+      return new SheetEditor(sheetNode);
+   }
+
+   private class SheetEditor extends JPanel {
+
+      SheetEditor(NeoNode sheetNode) {
+         super(new BorderLayout());
+
+         final JTable tblSheet = new JTable(new SheetTableModel(sheetNode.getNode()));
+         final TableColumnModel columnModel = tblSheet.getColumnModel();
+         for (int i = 0; i < columnModel.getColumnCount(); i++)
+            columnModel.getColumn(i).setMinWidth(50);
+         add(new JScrollPane(tblSheet), BorderLayout.CENTER);
+      }
+
+      private class SheetTableModel extends AbstractTableModel {
+
+         private final List<Row> content = new ArrayList<>();
+         private final Node sheetNode;
+
+         SheetTableModel(Node sheetNode) {
+
+            this.sheetNode = sheetNode;
+
+            final Map<Integer, Node> existingRows = new TreeMap<>();
+            final Map<Integer, Map<Integer, Node>> rowCells = new LinkedHashMap<>();
+            outgoingROW(sheetNode, (relationship, other) -> {
+
+               final Integer rowName = Integer.valueOf(getNameProperty(other));
+               existingRows.put(rowName, other);
+               rowCells.put(rowName, new LinkedHashMap<>());
+
+               outgoingCELL(other, (relationship1, cellNode) -> incomingCELL(cellNode, (relationship11, other1) -> {
+                  if (isColumn(other1)) {
+                     final Integer colName = Integer.valueOf(getNameProperty(other1));
+                     rowCells.get(rowName).put(colName, cellNode);
+                  }
+               }));
+            });
+
+            for (int i = 0; i < 50; i++) {
+               if (existingRows.containsKey(i))
+                  content.add(new Row(i, existingRows.get(i), rowCells.get(i)));
+               else content.add(new Row(i));
+            }
+         }
+
+         @Override
+         public int getRowCount() {
+            return content.size();
+         }
+
+         @Override
+         public int getColumnCount() {
+            return content.get(0).cells.size();
+         }
+
+         @Override
+         public Object getValueAt(int rowIndex, int columnIndex) {
+            return content.get(rowIndex).cells.get(columnIndex);
+         }
+
+         @Override
+         public String getColumnName(int column) {
+            return content.get(0).cells.get(column).name + "";
+         }
+
+         @Override
+         public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return true;
+         }
+
+         @Override
+         public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+            final Row row = content.get(rowIndex);
+            row.cells.get(columnIndex).setValue(aValue, row);
+            fireTableCellUpdated(rowIndex, columnIndex);
+         }
+
+         private final class Row {
+
+            private final int name;
+            private final List<Cell> cells = new ArrayList<>();
+            private Node node;
+
+            private Row(int name) {
+               this.name = name;
+               for (int i = 0; i < 20; i++)
+                  cells.add(new Cell(i, null));
+            }
+
+            public Row(int name, Node rowNode, Map<Integer, Node> cellNodes) {
+               this.name = name;
+               this.node = rowNode;
+
+               for (int i = 0; i < 20; i++) {
+                  if (cellNodes != null && cellNodes.containsKey(i)) {
+                     cells.add(new Cell(i, cellNodes.get(i)));
+                  } else {
+                     cells.add(new Cell(i, null));
+                  }
+               }
+            }
+
+            public Node getNode() {
+               if (node == null) {
+                  node = newRow();
+                  setNameProperty(node, Integer.toString(name));
+                  relateROW(sheetNode, node);
+               }
+               return node;
+            }
+         }
+
+         private final class Cell {
+
+            private final int name;
+            private String value;
+            private Node cellNode;
+
+            Cell(int name, Node cellNode) {
+               this.name = name;
+               this.cellNode = cellNode;
+               this.value = cellNode == null ? null : getValue(cellNode);
+            }
+
+            @Override
+            public String toString() {
+               return value;
+            }
+
+            public void setValue(Object aValue, Row row) {
+               getGraph().doInTransaction(new NeoModel.Committer() {
+                  @Override
+                  public void doAction(Transaction tx) throws Throwable {
+
+                     if (cellNode != null) {
+                        ExcelPlugin.setValue(cellNode, aValue);
+                        value = aValue == null ? null : aValue.toString();
+                        return;
+                     }
+
+                     final Node rowNode = row.getNode();
+                     final Node colNode = getColumnNode();
+
+                     final Node cellNode = newCell();
+                     ExcelPlugin.setValue(cellNode, aValue);
+                     value = aValue == null ? null : aValue.toString();
+                     relateCELL(rowNode, cellNode);
+                     relateCELL(colNode, cellNode);
+                  }
+
+                  @Override
+                  public void exception(Throwable throwable) {
+                     SwingUtil.showException(app, throwable);
+                  }
+               });
+            }
+
+            private Node getColumnNode() {
+
+               final Set<Node> found = new LinkedHashSet<>();
+               outgoingCOL(sheetNode, (relationship, other) -> {
+                  if (isColumn(other) && name == Integer.valueOf(getNameProperty(other)))
+                     found.add(other);
+               });
+
+               if (found.isEmpty()) {
+                  final Node colNode = newColumn();
+                  setNameProperty(colNode, Integer.toString(name));
+                  relateCOL(sheetNode, colNode);
+                  found.add(colNode);
+               }
+
+               return found.iterator().next();
+            }
+         }
+      }
    }
 }
