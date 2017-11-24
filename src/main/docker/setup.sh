a204955fe539c1f5d0b1d57364e319bc1c7b0cc9
sogern <sogern@gmail.com>
2017-11-24 15:00:48 +0100

    Fixed nginx log filter, refactored docker setup.sh

--- src/main/docker/setup.sh
+++ src/main/docker/setup.sh
@@ -10,22 +10,66 @@ export DOCKER_REGISTRY=localhost
 export TAG=dev
 export COMPOSE_FILES="-f docker-compose.yml -f docker-compose.override.yml"
 
-if [ ! -f elasticsearch/analysis-icu-5.6.3.zip ]; then
-  wget -P elasticsearch https://artifacts.elastic.co/downloads/elasticsearch-plugins/analysis-icu/analysis-icu-5.6.3.zip
-fi
-if [ ! -f elasticsearch/ingest-attachment-5.6.3.zip ]; then
-  wget -P elasticsearch https://artifacts.elastic.co/downloads/elasticsearch-plugins/ingest-attachment/ingest-attachment-5.6.3.zip
-fi
+build() {
+  echo -e "\n### BUILDING $1"
+  docker-compose ${COMPOSE_FILES} build $1
+}
 
-echo "### DOCKER-COMPOSE START"
+buildAndStart() {
+  name=$1[@]
+  containers=("${!name}")
+  for container in "${containers[@]}"; do
+    build ${container}
+    start ${container}
+  done
+}
 
-#declare -a containers=("stardog" "elasticsearch" "elasticsearchlogs" "kibana" "fluentd" "web")
-declare -a containers=("elasticsearchlogs" "kibana" "fluentd" "web")
+start() {
+  echo -e "\n### STARTING $1"
+  docker-compose ${COMPOSE_FILES} up -d $1
+}
 
-for container in "${containers[@]}"; do
-    echo "### BUILDING $container"
-    docker-compose ${COMPOSE_FILES} build ${container}
-    docker-compose ${COMPOSE_FILES} up -d ${container}
-done
+case $1 in
+  all)
+    declare -a containers=("stardog" "elasticsearch" "elasticsearchlogs" "kibana" "fluentd" "web")
+    buildAndStart containers
+    ;;
 
-echo "### DOCKER-COMPOSE DONE"
+  stardog | elasticsearchlogs | kibana | fluentd | web)
+    build $1
+    start $1
+    ;;
+
+  elasticsearch)
+    if [ ! -f elasticsearch/analysis-icu-5.6.3.zip ]; then
+      wget -P elasticsearch https://artifacts.elastic.co/downloads/elasticsearch-plugins/analysis-icu/analysis-icu-5.6.3.zip
+    fi
+    if [ ! -f elasticsearch/ingest-attachment-5.6.3.zip ]; then
+      wget -P elasticsearch https://artifacts.elastic.co/downloads/elasticsearch-plugins/ingest-attachment/ingest-attachment-5.6.3.zip
+    fi
+
+    build $1
+    start $1
+    ;;
+
+  efk)
+    declare -a containers=("elasticsearchlogs" "kibana" "fluentd" "web")
+    buildAndStart containers
+    ;;
+
+  list)
+    echo -e "Build options:\n
+  - all
+  - efk
+  - elasticsearch
+  - elasticsearchlogs
+  - kibana
+  - fluentd
+  - web
+  - stardog
+"
+    ;;
+  *)
+    echo -e "Usage:\n'$0 list' to display available options\n'$0 all' to build everything"
+    ;;
+esac
