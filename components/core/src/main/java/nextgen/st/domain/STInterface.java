0d701524588644fe96bc21c0c1c1c8662fae1ad7
geirove <geiroveenge@gmail.com>
2020-06-14 23:09:51 +1000

    Added STInterfaces, templates can now implement interfaces, to use as type-arguments. TestClasses are now generated to src/test/

---
+++ components/core/src/main/java/nextgen/st/domain/STInterface.java
@@ -0,0 +1,57 @@
+package nextgen.st.domain;
+
+public class STInterface {
+
+	private final io.vertx.core.json.JsonObject jsonObject;
+
+	public STInterface() { 
+		this.jsonObject = new io.vertx.core.json.JsonObject();
+		jsonObject.put("uuid", java.util.UUID.randomUUID().toString());
+	}
+
+	public STInterface(io.vertx.core.json.JsonObject jsonObject) { 
+		this.jsonObject = jsonObject;
+		java.lang.String uuidString = jsonObject.getString("uuid");
+		if (uuidString == null) jsonObject.put("uuid", java.util.UUID.randomUUID().toString());
+	}
+
+	public io.vertx.core.json.JsonObject getJsonObject() { 
+		return this.jsonObject;
+	}
+
+	public STInterface removeUuid() {
+		this.jsonObject.remove("uuid");
+		return this;
+	}
+
+	@Override
+	public boolean equals(java.lang.Object o) { 
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		final STInterface other = (STInterface) o;
+		return jsonObject.getString("uuid").equals(other.getJsonObject().getString("uuid"));
+	}
+
+	@Override
+	public int hashCode() { 
+		return java.util.Objects.hash(jsonObject.getString("uuid"));
+	}
+
+	public STInterface setName(String value) { 
+		jsonObject.put("name", value);
+		return this;
+	}
+
+	public String getName() { 
+		return jsonObject.getString("name");
+	}
+
+	public String getName(String defaultValue) { 
+		return jsonObject.getString("name", defaultValue);
+	}
+
+	@Override
+	public java.lang.String toString() { 
+		return jsonObject.encode();
+	}
+}
